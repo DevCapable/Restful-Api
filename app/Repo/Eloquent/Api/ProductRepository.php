60fef1c73565be28033c524bf6a7a03b5bc7b04a
DevCapable <jrasaq77@gmail.com>
2022-02-22 15:34:07 +0100

    update

--- app/Repo/Eloquent/Api/ProductRepository.php
+++ app/Repo/Eloquent/Api/ProductRepository.php
@@ -1,54 +1,43 @@
 <?php
+
 namespace App\Repo\Eloquent\Api;
+
 use App\Models\Product;
 use App\Repo\Eloquent\ProductRepositoryInterface;
-use Illuminate\Database\Eloquent\Model;
 
 class ProductRepository implements ProductRepositoryInterface
 {
 
-    private $model;
-
-    public function __construct(Model $model){
-        $this->model = $model;
-    }
-
     public function create($data)
     {
-    //   return Product::create([
-    //         'cost'=> $data->cost,
-    //         'product_name'=> $data->product_name,
-    //     ]);
-
-    return $this->model::create($data);
+      return Product::create([
+            'cost'=> $data->cost,
+            'product_name'=> $data->product_name,
+        ]);
     }
 
     public function update($data)
     {
-        // return Product::where('id',$data['id'])->update(['cost'=>$data['data']['cost'],
-        //             'product_name'=>$data['data']['product_name']]);
-
-        return $this->model::where('id',$data['id'])->update($data);
-
+        return Product::where('id',$data['id'])->update(['cost'=>$data['data']['cost'],
+                    'product_name'=>$data['data']['product_name']]);
     }
 
     public function findById($id)
     {
-        //    return  Product::where('id',$id)->first();
-           return $this->model::where('id',$id)->first();
+           return  Product::where('id',$id)->first();
 
     }
 
     public function destroy($product)
     {
-        $item = $this->model::where('id',$product->id)->first();
+        $item = Product::where('id',$product->id)->first();
 
         return $item->delete();
     }
 
     public function searchByName($name)
     {
-        return $this->model::where('product_name','like','%'.$name.'%')->get();
+        return Product::where('product_name','like','%'.$name.'%')->get();
 
     }
 }
